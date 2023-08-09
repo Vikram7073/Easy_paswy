6a96a9c476704592d1c9f29ae275f9a4f2c33a87
vikram Singh <vikram@appitronsolutions.com>
2023-08-09 23:54:08 +0530

    changes

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -24,30 +24,46 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Center(
         child: Padding(
           padding:  EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 50,),
-              Center(child: SvgPicture.asset("assets/images/app_logoImg.svg",height: 160,width: 160,)),
-              SizedBox(height: 50,),
-              Text("Email"),
-              SizedBox(height: 5,),
-
-              CustomField(fillColor: Colors.grey.withOpacity(0.4),),
-              SizedBox(height: 15,),
-              Text('Password'),
-              SizedBox(height: 5,),
-
-              CustomField(fillColor: Colors.grey.withOpacity(0.4),),
-              Spacer(),
-              CustomButton(text: "Login", onTap: () {  },),
-              SizedBox(height: 40,)
-
-
-
-
-
-            ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 50,),
+                Center(child: SvgPicture.asset("assets/images/app_logoImg.svg",height: 160,width: 160,)),
+                SizedBox(height: 50,),
+                Text("Email"),
+                SizedBox(height: 5,),
+                CustomField(fillColor: Colors.grey.withOpacity(0.4),),
+                SizedBox(height: 15,),
+                Text('Password'),
+                SizedBox(height: 5,),
+                CustomField(fillColor: Colors.grey.withOpacity(0.4),),
+                SizedBox(height: 50,),
+                CustomButton(text: "Login", onTap: () {  },),
+                SizedBox(height: 15,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Image.asset("assets/images/googleImage.png",height: 40,width: 40,),
+                    SizedBox(width: 8,),
+                    Text("Sign in with google")
+                  ],
+                ),
+                SizedBox(height: 15,),
+                Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(width: 11,),
+                    Image.asset("assets/images/facebookIconImg.png",height: 40,width: 40,),
+                    SizedBox(width: 8,),
+                    Text("Sign in with facebook")
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
